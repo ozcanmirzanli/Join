41602838f2ef75cf48d8fdb03224a9694d4d570b
Özcan Mirzanli <ozcanmirzanli@protonmail.com>
2024-04-22 13:31:00 +0200

    load animation added

---
+++ js/login.js
@@ -0,0 +1,7 @@
+var backgroundFade = document.getElementById("background-fade");
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(function () {
+    backgroundFade.style.display = "none";
+  }, 2000);
+});
