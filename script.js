231015ad4cdb48e7b0f06ea4e8385765a8491f36
Özcan Mirzanli <ozcanmirzanli@protonmail.com>
2024-04-23 16:12:45 +0200

    fixes for load animation

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-async function includeHTML(){
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+async function includeHTML() {
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
-    initSidemenu();
+  }
+  initSidemenu();
 }
 
-function initSidemenu(){
-    let clickedBtnId = localStorage.getItem('clickedBtnId');
-    if (clickedBtnId) {
-        let clickedBtn = document.getElementById(clickedBtnId);
-        clickedBtn.classList.add('clickedSideBtn');
-    }
+function initSidemenu() {
+  let clickedBtnId = localStorage.getItem("clickedBtnId");
+  if (clickedBtnId) {
+    let clickedBtn = document.getElementById(clickedBtnId);
+    clickedBtn.classList.add("clickedSideBtn");
+  }
 }
