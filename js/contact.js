966be4042511da29d66ca5017282b4ec1ec32b15
Anna Brandes <annalxd@googlemail.com>
2024-05-06 00:21:21 +0200

    contact

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,8 @@
+let contact = [];
+
 function addNewConatct(){
     let addContact = document.getElementById("addContact");
+    addContact.classList.remove("d-none");
     addContact.classList.add("addContact");
     addContact.innerHTML = /*html*/`
             <section class="addContactLeft">
@@ -7,18 +10,35 @@ function addNewConatct(){
                 <h1>AddContact</h1><h2>Tasks are better with a Team!</h2><div class="vector"></div>
             </section>
             <section class="addContactRigth">
+            <img src="./assets/img/Close.png" alt="" class="close">
             <div class="inputarea">
                 <img src="./assets/img/Group 13.png" alt="" class="addInitials">
             <div class="inputFields">
-                <div class="input"><input type="text" placeholder="Name"><img src="./assets/img/input_name.png" alt=""></div>
-                <div class="input"><input type="e-mail" placeholder="E-Mail"><img src="./assets/img/mail.png" alt=""></div>
-                <div class="input"><input type="number" placeholder="Telefonnummer"><img src="./assets/img/call.png" alt=""></div>
+                <div class="input"><input type="text" placeholder="Name" id="name"><img src="./assets/img/input_name.png" alt="" class="inputImg"></div>
+                <div class="input"><input type="e-mail" placeholder="E-Mail" id="mail"><img src="./assets/img/mail.png" alt="" class="inputImg"></div>
+                <div class="input"><input type="number" placeholder="Telefonnummer" id="number"><img src="./assets/img/call.png" alt="" class="inputImg"></div>
             </div>
             </div>
-                <div class="buttonArea">
-                    <img src="./assets/img/cancel.png" alt="" class="cancel">
-                    <img src="./assets/img/Primary check button.png" alt="" class="create">
+            <div class="buttonArea">
+                    <img src="./assets/img/cancel.png" alt="" class="cancel" class="btnCancel" onclick="close()">
+                    <img src="./assets/img/Primary check button.png" alt="" class="create" class="btnCreate" onclick="saveContact()">
                 </div>
             </section>      
     `
+}
+
+async function saveContact(){
+    let name = document.getElementById("name").value;
+    let mail = document.getElementById("mail").value;
+    let number = document.getElementById("number").value;
+
+    contact.push({ name: name.value, email: mail.value, number: number.value });
+  await setItem("contact", JSON.stringify(contact));
+}
+
+function close(){
+    let addContact = document.getElementById("addContact");
+    addContact.innerHTML = '';
+    addContact.classList.add("d-none");
+    addContact.classList.remove("addContact");
 }
