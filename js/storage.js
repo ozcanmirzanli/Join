5ef8da46f12f0207b65c06dc39a909c41c58d7fa
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-05-07 12:34:43 +0200

    JSDoc comments for storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,6 +1,15 @@
 const STORAGE_TOKEN = "CDWXOVPDT01MKZ60CIJW2K95MBKWUP1IFUHRH575";
 const STORAGE_URL = "https://remote-storage.developerakademie.org/item";
 
+/**
+ * Stores an item in remote storage.
+ *
+ * This function sends a POST request to the remote storage service to save a key-value pair.
+ * It uses a predefined storage token for authentication.
+ *
+ * @async
+ * @param {string} key - The key under which the value will be stored.
+ */
 async function setItem(key, value) {
   const payload = { key, value, token: STORAGE_TOKEN };
   return fetch(STORAGE_URL, {
@@ -9,6 +18,15 @@ async function setItem(key, value) {
   }).then((res) => res.json());
 }
 
+/**
+ * Retrieves an item from remote storage.
+ *
+ * This function sends a GET request to the remote storage service to fetch the value associated
+ * with a specified key. It uses a predefined storage token for authentication.
+ *
+ * @async
+ * @param {string} key - The key for which the value needs to be retrieved.
+ */
 async function getItem(key) {
   const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
   return fetch(url)
@@ -16,5 +34,6 @@ async function getItem(key) {
     .then((res) => res.data.value);
 }
 
+// Attaching functions to window object for global accessibility
 window.setItem = setItem;
 window.getItem = getItem;
