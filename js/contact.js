253a7f061e9f8944e79e0da7f0237caca4ee1fad
Anna Brandes <annalxd@googlemail.com>
2024-05-07 03:02:36 +0200

    contacts
    add contact ready, push data, pull data

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,36 @@
-let contact = [];
+let contacts = [];
 
-async function getContact(){
-  try {
-   contact = JSON.parse(await getItem("contact"));
-  } catch (error) {
-    console.info('Could not load contacts')
-  } 
+let colors = [
+  "#FF7A002",
+  "#9327FF",
+  "#6E52FF",
+  "#FC71FF",
+  "#FFBB2B",
+  "rgb(31,215,193)",
+  "rgb(70,47,138)",
+  "rgb(255,70,70)",
+  "rgb(0,190,232)",
+  "rgb(255,122,0)",
+];
+
+async function init() {
+  await getContact();
+  renderContacts();
 }
 
+async function getContact() {
+  try {
+    contacts = JSON.parse(await getItem("contact"));
+  } catch (error) {
+    console.info("Could not load contacts");
+  }
+}
 
-function addNewConatct(){
-    let addContact = document.getElementById("addContact");
-    addContact.classList.remove("d-none");
-    addContact.classList.add("addContact");
-    addContact.innerHTML = /*html*/`
+function addNewConatct() {
+  let addContact = document.getElementById("addContact");
+  addContact.classList.remove("d-none");
+  addContact.classList.add("addContact");
+  addContact.innerHTML = /*html*/ `
             <section class="addContactLeft">
                 <img src="./assets/img/joinLogoWhite.svg" alt="" class="logo">
                 <h1>AddContact</h1><h2>Tasks are better with a Team!</h2><div class="vector"></div>
@@ -29,26 +46,77 @@ function addNewConatct(){
             </div>
             </div>
             <div class="buttonArea">
-                    <img src="./assets/img/cancel.png" alt="" class="cancel" class="btnCancel" onclick="close()">
+                    <img src="./assets/img/cancel.png" alt="" class="cancel" class="btnCancel" onclick="closeAddContact()">
                     <img src="./assets/img/Primary check button.png" alt="" class="create" class="btnCreate" onclick="saveContact()">
                 </div>
             </section>      
-    `
+    `;
 }
 
-async function saveContact(){
-    let name = document.getElementById("name").value;
-    let mail = document.getElementById("mail").value;
-    let number = document.getElementById("number").value;
+async function saveContact() {
+  let name = document.getElementById("name").value;
+  let mail = document.getElementById("mail").value;
+  let number = document.getElementById("number").value;
+  let color = colors[Math.floor(Math.random() * colors.length)];
 
-    contact.push({ name: name, email: mail, number: number});
-    await setItem("contact", JSON.stringify(contact));
-    close();
+  contacts.push({ name: name, email: mail, number: number, color: color });
+  await setItem("contact", JSON.stringify(contacts));
+  closeAddContact();
 }
 
-function close(){
-    let addContact = document.getElementById("addContact");
-    addContact.innerHTML = '';
-    addContact.classList.add("d-none");
-    addContact.classList.remove("addContact"); 
+function closeAddContact() {
+  let addContact = document.getElementById("addContact");
+  addContact.innerHTML = "";
+  addContact.classList.add("d-none");
+  addContact.classList.remove("addContact");
+}
+
+function renderContacts() {
+  let overview = document.getElementById("allContacts");
+
+  contacts.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+
+  for (let index = 0; index < contacts.length; index++) {
+    const contact = contacts[index];
+    const initials = getInitials(contact.name);
+
+    overview.innerHTML += /*html*/ `
+    <div class="contactSmall" onclick="getOverview(contacts[${index}])">
+            <div class="initials" style="background-color: ${contact.color};">${initials}</div>
+            <div class="contactInfo">
+                <div class="name">${contact["name"]}</div>
+                <div class="mail">${contact["email"]}</div>
+  </div>
+           </div>
+        `;
+  }
+}
+
+function getInitials(name) {
+  return name
+    .split(" ")
+    .map((word) => word.charAt(0))
+    .join("");
+}
+
+contacts.forEach(function (contact) {
+  contact.initials = getInitials(contact.name);
+});
+
+function getOverview(contact){
+    document.getElementById("contactBig").classList.remove('d-none')
+    document.getElementById("contactBig").innerHTML =/*html*/`
+        <div class="bigInitials" style="background-color: ${contact["color"]};">${initials}</div>
+            <div class="">
+                <h3 class="bigName">${contact["name"]}</h3>
+                <div class="btnArea">
+                <img src="./assets/img/Delete contact.svg" alt="delete" onclick="deletContact()"> 
+          <img src="./assets/img/edit contacts.svg" alt="edit" onclick="editContact()">
+</div>
+          <h2>Contact Information</h2>
+                <div><h4>E-Mail</h4>${contact["email"]}</div>
+                <div><h4>Phone</h4>${contact["number"]}</div>
+  </div>   `
 }
