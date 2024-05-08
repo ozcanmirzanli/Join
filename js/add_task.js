54af555d9de6fc45ed84d59326c1bb03d776b3f3
Martin Zehmisch <Martin.Zehmisch@web.de>
2024-05-08 15:59:20 +0200

    Update add_task.js

--- js/add_task.js
+++ js/add_task.js
@@ -337,8 +337,8 @@ function getassignListHTML(contact, badgeColor, i) {
     return /*HTML*/ `
             <div id="contact${i}" onclick="assignContact(${i}, '${contact.name}')">
                 <div>
-                    <div class="assignToBadge" style="background-color: ${badgeColor}</div>
-                    <div class="contactName">${contact.name}</div>
+                    <div class="assignToBadge" style="background-color: ${badgeColor}"></div>
+                    <div class="">${contact.name}</div>
                 </div>
                 <img id="checkbox${i}" src="./assets/img/addTask_AssignTo_Checkbox.svg" class="checkbox">
             </div>
