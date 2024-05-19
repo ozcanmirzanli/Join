6d471a367dc27647aeaca37e686efc7237e3fef0
Özcan Mirzanli <112502991+ozcanmirzanli@users.noreply.github.com>
2024-05-19 13:31:15 +0200

    add contact pop-up css redesign

--- js/contact.js
+++ js/contact.js
@@ -28,9 +28,10 @@ async function getContact() {
 }
 
 function addNewContact() {
-  let addContact = document.getElementById("addContact");
-  addContact.classList.remove("d-none");
-  addContact.classList.add("addContact");
+  let addContact = document.getElementById("add-contact-bg");
+
+  addContact.style.display = "flex";
+
   let addBtn = document.querySelector(".addBtn");
   addBtn.style.backgroundColor = "rgb(9,25,49)";
   addContact.innerHTML = generateAddContactHTML();
@@ -38,40 +39,49 @@ function addNewContact() {
 
 function generateAddContactHTML() {
   return /*html*/ `
-  <section class="addContactLeft">
+  <div class="add-contact-container">
+  <div class="add-contact-left">
+    <div class="add-contact-left-container">
   <img src="./assets/img/joinLogoWhite.svg" alt="" class="logo">
-  <h1>AddContact</h1><h2>Tasks are better with a Team!</h2><div class="vector"></div>
-  </section>
-  <section class="addContactRight">
-  <div class="close"><img src="./assets/img/Close.png" alt="" onclick="closeAddContact()"></div>
-  <div class="inputarea">
-  <img src="./assets/img/Group 13.png" alt="" class="addInitials">
+  <h1>AddContact</h1><h2>Tasks are better with a Team!</h2>
+  <div class="vector"></div>
+  </div>
+  </div>
+  <div class="add-contact-right">
+  <div class="add-contact-close-btn"><img src="./assets/img/Close.png" alt="" onclick="closeAddContact()"></div>
+
+    <div class="add-contact-right-container">
+  <div class="input-area">
+  <img src="./assets/img/Group 13.png" alt="" class="contact-picture">
   ${generateAddContactInputsHTML()}
   </div>
-  ${generateAddContactButtonsHTML()}`;
+  </div>
+  </div>`;
 }
 
 function generateAddContactInputsHTML() {
   return /*html*/ `
-  <form class="inputFields">
-  <div class="input"><input type="text" placeholder="Name" id="name"><img src="./assets/img/input_name.png" alt="" class="inputImg"></div>
-  <div class="input"><input type="e-mail" placeholder="E-Mail" id="mail"><img src="./assets/img/mail.png" alt="" class="inputImg"></div>
-  <div class="input"><input type="number" placeholder="Telefonnummer" id="number"><img src="./assets/img/call.png" alt="" class="inputImg"></div>
+  <form class="input-fields">
+  <div class="input-container"><input type="text" placeholder="Name" id="name"><img src="./assets/img/input_name.png" alt="" class="inputImg"></div>
+  <div class="input-container"><input type="e-mail" placeholder="Email" id="mail"><img src="./assets/img/mail.png" alt="" class="inputImg"></div>
+  <div class="input-container"><input type="number" placeholder="Phone" id="number"><img src="./assets/img/call.png" alt="" class="inputImg"></div>
+  ${generateAddContactButtonsHTML()};
+
   </form>`;
 }
 
 function generateAddContactButtonsHTML() {
   return /*html*/ `
-  <div class="btnArea" style= "margin-top: 80px;" >
-     <button class="addContactCancel" onclick="closeAddContact()">
+  <div class="add-contact-btns"  >
+     <button class="add-contact-cancel" onclick="closeAddContact()">
      <p class="cancelText">Cancel</p><img src="./assets/img/Close.png" alt="" class="close">
      </button>
-     <button class="addContactSave" onclick="saveContact()">
+     <button class="add-contact-save" onclick="saveContact()">
      <p class="saveText">Create Contact</p>
     <img src="./assets/img/check.png" alt="" style="width: 20px; height: 18px;"/>
     </button>
   </div>
-  </section> 
+  </div> 
   `;
 }
 
@@ -111,9 +121,9 @@ function addFirstLetter(name) {
 }
 
 function closeAddContact() {
-  let addContact = document.getElementById("addContact");
+  let addContact = document.getElementById("add-contact");
   addContact.classList.add("d-none");
-  addContact.classList.remove("addContact");
+  addContact.classList.remove("add-contact");
 }
 
 function renderContacts() {
@@ -236,7 +246,7 @@ function generateContactNameAreaHTML(contact) {
   return /*html*/ `
   <div class="nameArea">
   <div class="nameBig">${contact.name}</div>
-  <div class="btnArea">
+  <div class="add-contact-btns">
     <div class="deleteBtnContact" onclick="editContact(${contact.id})">
       <img src="./assets/img/edit.svg" alt="" class="imgEdit"/>Edit
     </div>
@@ -313,23 +323,22 @@ function editContact(contactId) {
 }
 
 function displayEditContactForm(contact) {
-  let addContact = document.getElementById("addContact");
-  addContact.classList.remove("d-none");
-  addContact.classList.add("addContact");
+  let addContact = document.getElementById("add-contact-bg");
+  addContact.style.display = "flex";
   addContact.innerHTML = generateEditContactHTML(contact);
   document.getElementById("badgeColor").value = contact.color;
 }
 
 function generateEditContactHTML(contact) {
   return /*html*/ `
-    <section class="addContactLeft">
+    <section class="add-contact-left">
       <img src="./assets/img/joinLogoWhite.svg" alt="" class="logo">
       <h1>Edit Contact</h1><div class="vector"></div>
     </section>
-    <section class="addContactRigth">
+    <section class="add-contact-right">
       <div class="close"><img src="./assets/img/Close.png" alt="" onclick="closeAddContact()"></div>
-      <div class="inputarea">
-        <img src="./assets/img/Group 13.png" alt="" class="addInitials">
+      <div class="input-area">
+        <img src="./assets/img/Group 13.png" alt="" class="contact-picture">
         ${generateEditContactInputsHTML(contact)}
       </div>
       ${generateEditContactButtonsHTML(contact.id)}
@@ -338,18 +347,18 @@ function generateEditContactHTML(contact) {
 
 function generateEditContactInputsHTML(contact) {
   return /*html*/ `
-    <div class="inputFields">
-      <div class="input">
+    <div class="input-fields">
+      <div class="input-container">
         <input type="text" placeholder="Name" id="name" value="${contact.name}">
-        <img src="./assets/img/input_name.png" alt="" class="inputImg">
+        <img src="./assets/img/input_name.png" alt="" class="input-img">
       </div>
-      <div class="input">
+      <div class="input-container">
         <input type="e-mail" placeholder="E-Mail" id="mail" value="${contact.email}">
-        <img src="./assets/img/mail.png" alt="" class="inputImg">
+        <img src="./assets/img/mail.png" alt="" class="input-img">
       </div>
-      <div class="input">
+      <div class="input-container">
         <input type="number" placeholder="Telefonnummer" id="number" value="${contact.number}">
-        <img src="./assets/img/call.png" alt="" class="inputImg">
+        <img src="./assets/img/call.png" alt="" class="input-img">
       </div>
     </div>`;
 }
