4725d588016a5b22452ed923f6db3a5da8f61ec5
Martin Zehmisch <Martin.Zehmisch@web.de>
2024-06-04 18:43:49 +0200

    working on AddTask

--- js/addTaskMain.js
+++ js/addTaskMain.js
@@ -24,26 +24,18 @@ function handleCategoryChange(selectElement) {
     // console.log("Selected category: " + selectedCategory);
   }
 
-  /**
- * Handles input focus event.
- */
-function handleInputFocus() {
-    let subtasksInput = document.querySelector("addSubtaskMain");
-    subtasksInput.style.borderColor = "rgba(41, 171, 226, 1)";
-  }
-
-  /**
+/**
  * Handles focus event on subtask input.
  */
 function handleFocus() {
-    let addSubtaskMain = document.querySelector(".addSubtaskMain");
-    addSubtaskMain.style.border = "1px solid rgba(41, 171, 226, 1)";
+    let addSubtaskMain = document.getElementById(".addSubtaskMain");
+    addSubtaskMain.style.border = "1px solid #29abe2";
   }
   
   /**
    * Handles blur event on subtask input.
    */
   function handleBlur() {
-    let addSubtaskMain = document.querySelector(".addSubtaskMain");
-    addSubtaskMain.style.border = "1px solid rgba(209, 209, 209, 1)";
+    let addSubtaskMain = document.getElementById(".addSubtaskMain");
+    addSubtaskMain.style.border = "1px solid #29abe2"
   }
